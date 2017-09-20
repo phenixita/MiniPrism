758d04387e6abe16f80c6a590e5998353a4cdc27
Michele Ferracin <michele.ferracin@madlab.biz>
2017-09-20 09:50:47 +0200

    Init

---
+++ MiniPrism/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using MiniPrism.Views;
+using Prism.Unity;
+using System.Windows;
+
+namespace MiniPrism
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
